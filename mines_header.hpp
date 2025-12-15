c1978549de24f7241bf28bfc786d9a6131cf0c64
root <root@localhost>
2025-12-15 20:44:13 +0530

    mfkfkf

--- mines_header.hpp
+++ mines_header.hpp
@@ -4,7 +4,7 @@
 
 
 
-extern int A[5][5];  // Declaration of the array
+extern int A[5][5];  // Declaration of th
 void arr_initialization(int A[][5]);
 void arr_display(int A[][5]);
 int mine_generator();
